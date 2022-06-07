9e56b3d97d9601491c006e9cf034a2a0b5975d1d
Graeme Montrose <teentrose@gmail.com>
2022-06-06 22:12:31 -0600

    added majority of code still waiting on auth troubleshooting

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import {useMutation} from '@apollo/client'
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import {SAVE_BOOK} from '../utils/mutations'
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -14,6 +15,8 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  const [saveBook, {error}] = useMutation(SAVE_BOOK)
+
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -23,7 +26,10 @@ const SearchBooks = () => {
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
+    const searchGoogleBooks = (query) => {
+      return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+    };
+    
     if (!searchInput) {
       return false;
     }
@@ -44,7 +50,7 @@ const SearchBooks = () => {
         description: book.volumeInfo.description,
         image: book.volumeInfo.imageLinks?.thumbnail || '',
       }));
-
+ 
       setSearchedBooks(bookData);
       setSearchInput('');
     } catch (err) {
@@ -56,18 +62,19 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    // console.log(bookToSave)
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
+    
     if (!token) {
+      console.log("Something's wrong with the token")
       return false;
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
+      const {data} = await saveBook({variables: {input: {...bookToSave}}});
+      console.log(data)
+      if (!data.ok) {
         throw new Error('something went wrong!');
       }
 
